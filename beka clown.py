5eb9b4519982c6f238ff405afcd8aafcf342f4e1
origamitoy <119298463+origamitoy@users.noreply.github.com>
2022-11-28 16:45:52 +0600

    Add files via upload

---
+++ beka clown.py
@@ -0,0 +1,15 @@
+#4
+dictionary = {
+
+  "class": "10b",
+  "Age": "16y.old",
+  "year": 2006
+}
+dictionary.pop("Age")
+print(dictionary)
+#3
+b = int(input("Enter a number: "))
+if b%2==0:
+    print("even")
+else:
+    print("odd")
