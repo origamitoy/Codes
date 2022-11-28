cda62fffbc6573cc0397240e6edfc3e57dc24667
origamitoy <119298463+origamitoy@users.noreply.github.com>
2022-11-28 17:24:06 +0600

    Add files via upload

---
+++ oriproject/main.py
@@ -0,0 +1,72 @@
+print("Hello world")
+
+x = 4
+y = 6
+print(4+6)
+
+if y > x:
+    print("y you stupid :skull:")
+
+#This is a comment
+
+"""
+this is a comment 
+with more than 
+one line
+"""
+
+z = "John"
+print(z)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+a = 4
+A = "DAN"
+#A will not overwrite a
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = "John"
+print(x, y)
+
+i = 1    # int
+o = 2.8  # float
+p = 1j   # complex
+print(type(i))
+print(type(o))
+print(type(p))
+
+l = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(l) #a multi lined string
+
+for n in "banana":
+  print(n)
+
+a = "Hello, World!"
+print(len(a)) #Length
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
